f512e613359082e63f2b9c5905cd5e5d8ee2fab1
dermio <thisdjay@gmail.com>
2018-02-20 19:07:22 -0800

    Added test for Game component

---
+++ src/components/game.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {shallow, mount} from "enzyme";
+
+import Game from "./game";
+
+describe("<Game />", () => {
+  it("Renders without crashing", () => {
+    shallow(<Game />);
+  });
+
+  it("Can make guesses", () => {
+    let wrapper = shallow(<Game />);
+
+    wrapper.setState({
+      correctAnswer: 50
+    });
+
+    wrapper.instance().makeGuess(25);
+    expect(wrapper.state("guesses")).toEqual([25]);
+    expect(wrapper.state("feedback")).toEqual("You're Warm.");
+
+    wrapper.instance().makeGuess(80);
+    expect(wrapper.state("guesses")).toEqual([25, 80]);
+    expect(wrapper.state("feedback")).toEqual("You're Cold...");
+
+  });
+
+});
